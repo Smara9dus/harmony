b2c6e11d69a1b3db9d4e89a137cdded3a1226969
Smara9dus <emeraldbay53@gmail.com>
2019-04-01 14:34:34 -0500

    Camera changes

--- harmony.js
+++ harmony.js
@@ -3,14 +3,14 @@ var config = {
     width: 960,
     height: 675,
     parent: 'phaser-example',
-    pixelArt: true,
+    pixelArt: false,
     physics: {
         default: 'matter',
         matter: {
             gravity: {
                 y: 1
             },
-            debug: true
+            debug: true //set this to false later
         }
     },
     scene: {
@@ -30,7 +30,7 @@ function create ()
 
     this.matter.world.setBounds(0, 0, worldWidth, worldHeight);
 
-    //  Create loads of random bodies
+    //  Create loads of random bodies (this will have to be moved at some point)
     for (var i = 0; i < 30; i++)
     {
         var x = Phaser.Math.Between(0, worldWidth);
@@ -40,38 +40,38 @@ function create ()
 
     }
 
-    this.matter.add.mouseSpring();
+    this.matter.add.mouseSpring(); // makes blocks draggable!
 
     var cursors = this.input.keyboard.createCursorKeys();
 
     var controlConfig = {
         camera: this.cameras.main,
-        left: cursors.left,
-        right: cursors.right,
-        up: cursors.up,
-        down: cursors.down,
-        zoomIn: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q),
-        zoomOut: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E),
-        acceleration: 0.06,
-        drag: 0.0005,
-        maxSpeed: 1.0
+        // left: cursors.left,
+        // right: cursors.right,
+        // up: cursors.up,
+        // down: cursors.down,
+        // zoomIn: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q),
+        // zoomOut: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E),
+        // acceleration: 0.06,
+        // drag: 0.0005,
+        // maxSpeed: 1.0
     };
 
     controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
-
-    this.input.keyboard.on('KEY_DOWN_Z', function (event) {
-
-        this.cameras.main.rotation += 0.01;
-
-    }, 0, this);
-
-    this.input.keyboard.on('KEY_DOWN_X', function (event) {
-
-        this.cameras.main.rotation -= 0.01;
-
-    }, 0, this);
-
-    var cam = this.cameras.main;
+    //
+    // this.input.keyboard.on('KEY_DOWN_Z', function (event) {
+    //
+    //     this.cameras.main.rotation += 0.01;
+    //
+    // }, 0, this);
+    //
+    // this.input.keyboard.on('KEY_DOWN_X', function (event) {
+    //
+    //     this.cameras.main.rotation -= 0.01;
+    //
+    // }, 0, this);
+    //
+    // var cam = this.cameras.main;
 
     //gui = new dat.GUI();
 
@@ -80,11 +80,11 @@ function create ()
     //p1.add(this.input, 'y').listen();
     //p1.open();
 
-    var help = {
-        line1: 'Cursors to move',
-        line2: 'Q & E to zoom',
-        line3: 'Z & X to rotate',
-    }
+    // var help = {
+    //     line1: 'Cursors to move',
+    //     line2: 'Q & E to zoom',
+    //     line3: 'Z & X to rotate',
+    // }
 
     // var f1 = gui.addFolder('Camera');
     // f1.add(cam, 'x').listen();
