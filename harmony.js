60c08c821682a2d538741b1553422034434f3894
Smara9dus <emeraldbay53@gmail.com>
2019-04-08 16:05:40 -0500

    Using image obj instead of rect
    Code is messy but final thing looks nicer

--- harmony.js
+++ harmony.js
@@ -4,39 +4,51 @@ var config = {
     height: 675,
     parent: 'phaser-example',
     pixelArt: false,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    },
     physics: {
         default: 'matter',
         matter: {
             gravity: {
                 y: 1
             },
-            debug: true //set this to false later
+            debug: false //set this to false later
         }
     },
-    scene: {
-        create: create,
-        update: update
-    }
 };
-
-var controls;
 
 var game = new Phaser.Game(config);
 
+let worldWidth;
+let worldHeight;
+
+function preload() {
+  this.load.image('block', 'assets/block.png');
+}
+
+var controls; // might be able to delete this when figuring out the fixed camera thing
+let blocks; // a collection of all objects currently on screen (Make this
+            // a separate object with its own methods for calculations)
+
 function create ()
 {
-    var worldWidth = 960;
-    var worldHeight = 675;
+    worldWidth = 960;
+    worldHeight = 675;
 
     this.matter.world.setBounds(0, 0, worldWidth, worldHeight);
 
-    //  Create loads of random bodies (this will have to be moved at some point)
+    blocks = this.add.group();
+
+    //  Create loads of random bodies (this will have to be removed at some point)
     for (var i = 0; i < 30; i++)
     {
         var x = Phaser.Math.Between(0, worldWidth);
         var y = Phaser.Math.Between(0, worldHeight);
 
-        this.matter.add.rectangle(x, y, 120, 30, { restitution: 0.9 });
+        this.matter.add.image(x,y,'block',{ restitution: 0.9 });
 
     }
 
@@ -46,57 +58,28 @@ function create ()
 
     var controlConfig = {
         camera: this.cameras.main,
-        // left: cursors.left,
-        // right: cursors.right,
-        // up: cursors.up,
-        // down: cursors.down,
-        // zoomIn: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q),
-        // zoomOut: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E),
-        // acceleration: 0.06,
-        // drag: 0.0005,
-        // maxSpeed: 1.0
     };
 
     controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
-    //
-    // this.input.keyboard.on('KEY_DOWN_Z', function (event) {
-    //
-    //     this.cameras.main.rotation += 0.01;
-    //
-    // }, 0, this);
-    //
-    // this.input.keyboard.on('KEY_DOWN_X', function (event) {
-    //
-    //     this.cameras.main.rotation -= 0.01;
-    //
-    // }, 0, this);
-    //
-    // var cam = this.cameras.main;
+}
 
-    //gui = new dat.GUI();
+function OnMouseClick (x,y){
+  this.matter.add.rectangle(x, y, 120, 30, { restitution: 0.9 });
+  console.log("Hi")
+}
 
-    //var p1 = gui.addFolder('Pointer');
-    //p1.add(this.input, 'x').listen();
-    //p1.add(this.input, 'y').listen();
-    //p1.open();
+function fixLocation(item) {
 
-    // var help = {
-    //     line1: 'Cursors to move',
-    //     line2: 'Q & E to zoom',
-    //     line3: 'Z & X to rotate',
-    // }
-
-    // var f1 = gui.addFolder('Camera');
-    // f1.add(cam, 'x').listen();
-    // f1.add(cam, 'y').listen();
-    // f1.add(cam, 'scrollX').listen();
-    // f1.add(cam, 'scrollY').listen();
-    // f1.add(cam, 'rotation').min(0).step(0.01).listen();
-    // f1.add(cam, 'zoom', 0.1, 2).step(0.1).listen();
-    // f1.add(help, 'line1');
-    // f1.add(help, 'line2');
-    // f1.add(help, 'line3');
-    // f1.open();
+    // Move the items when it is already dropped.
+    if (item.x < 90) {
+        item.x = 90;
+    }
+    else if (item.x > 180 && item.x < 270) {
+        item.x = 180;
+    }
+    else if (item.x > 360) {
+        item.x = 270;
+    }
 
 }
 
