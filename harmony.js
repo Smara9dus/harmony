b1589602c1f666eb085681467efea35a1872c329
Smara9dus <emeraldbay53@gmail.com>
2019-04-09 10:36:26 -0500

    Cleaned up a bit

--- harmony.js
+++ harmony.js
@@ -1,13 +1,14 @@
 var config = {
-    type: Phaser.WEBGL,
+    type: Phaser.AUTO,
     width: 960,
     height: 675,
+    backgroundColor: 0x000000,
     parent: 'phaser-example',
     pixelArt: false,
     scene: {
         preload: preload,
-        create: create,
-        update: update
+        create: create//,
+        //update: update // overriding the update function might be necessary later
     },
     physics: {
         default: 'matter',
@@ -15,32 +16,34 @@ var config = {
             gravity: {
                 y: 1
             },
-            debug: false //set this to false later
+            debug: true, //set this to false later
+            debugBodyColor: 0xffffff
         }
     },
 };
 
 var game = new Phaser.Game(config);
 
-let worldWidth;
-let worldHeight;
+const worldWidth = 960;
+const worldHeight = 675;
 
 function preload() {
   this.load.image('block', 'assets/block.png');
+  this.load.image('base', 'assets/base.png');
 }
 
-var controls; // might be able to delete this when figuring out the fixed camera thing
+//var controls; // might be able to delete this when figuring out the fixed camera thing
 let blocks; // a collection of all objects currently on screen (Make this
             // a separate object with its own methods for calculations)
+let base;
+let pauseButton;
 
 function create ()
 {
-    worldWidth = 960;
-    worldHeight = 675;
-
-    this.matter.world.setBounds(0, 0, worldWidth, worldHeight);
-
+    //base = this.matter.add.image(worldWidth/4,worldHeight-50,'base',{ isStatic: true });
     blocks = this.add.group();
+    base = this.matter.add.rectangle(worldWidth/4,worldHeight-50,worldWidth/2,100,{ isStatic: true });
+    pauseButton = this.matter.add.rectangle(worldWidth-30,30,30,30,{ isStatic: true });
 
     //  Create loads of random bodies (this will have to be removed at some point)
     for (var i = 0; i < 30; i++)
@@ -48,26 +51,19 @@ function create ()
         var x = Phaser.Math.Between(0, worldWidth);
         var y = Phaser.Math.Between(0, worldHeight);
 
-        this.matter.add.image(x,y,'block',{ restitution: 0.9 });
-
+        this.matter.add.rectangle(x,y,100,20,{ restitution: 0 });
     }
 
-    this.matter.add.mouseSpring(); // makes blocks draggable!
+    this.matter.add.mouseSpring(); // makes blocks draggable! Will neeed to be changed later
 
-    var cursors = this.input.keyboard.createCursorKeys();
-
-    var controlConfig = {
-        camera: this.cameras.main,
-    };
-
-    controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
+    // var controlConfig = {
+    //     camera: this.cameras.main,
+    // };
+    //
+    // controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
 }
 
-function OnMouseClick (x,y){
-  this.matter.add.rectangle(x, y, 120, 30, { restitution: 0.9 });
-  console.log("Hi")
-}
-
+// will be modified and used later for fixing location on screen
 function fixLocation(item) {
 
     // Move the items when it is already dropped.
@@ -83,7 +79,9 @@ function fixLocation(item) {
 
 }
 
-function update (time, delta)
-{
-    controls.update(delta);
-}
+
+
+// function update (time, delta)     // we aren't overriding this function right now
+// {
+//     controls.update(delta);
+// }
